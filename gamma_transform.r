343df31a437170cb2c71f9614b0489e879bf96cc
alberthaptic <albert@haptic.finance>
2023-02-23 15:27:37 -0400

    parametrize, etc.

--- gamma_transform.r
+++ gamma_transform.r
@@ -3,13 +3,18 @@ path = getwd()
 source(paste(path, "/install.r", sep=""))
 source(paste(path, "/config.r", sep=""))
 
-run <- function(P, Pa, Pb) {
 
+run <- function(N) {
     # Gamma transform
     # From https://lambert-guillaume.medium.com/gamma-transforms-how-to-hedge-squeeth-using-uni-v3-da785cb8b378
-
+   
+    Pa <- tick_base^(tick_index)
+    Pb <- (tick_base^(tick_index)) + (min_tick_dollar_value)
+    
     range_factor <- sqrt(Pb / Pa)
     K <- sqrt(Pb * Pa)
+
+    P <- K
 
     V_P <- (2 * (sqrt((K * range_factor) * P))) - K - P / range_factor -1
     Delta_P <- sqrt( (K * range_factor) / P) - 1 / range_factor -1
@@ -18,8 +23,9 @@ run <- function(P, Pa, Pb) {
     strikes <- c()
     gammas <- c()
 
-    r0 <- range_factor^(1/N)
+    r0 <- range_factor^(1 / N)
 
+    total_gamma <- 0
 
     for (i in 1:N) {
 
@@ -30,21 +36,12 @@ run <- function(P, Pa, Pb) {
         Delta_P <- (sqrt( (strikes[i] * r0) / P) - 1) / (r0 -1)
         Gamma_P <- -sqrt(K * r0) / (2 * (r0 - 1)) * P^(-3 / 2)
         gammas[i] <- Gamma_P
+        total_gamma <- total_gamma + Gamma_P
 
-        print(glue::glue("Position {i}: {liq} Delta_P: {Delta_P} Gamma_P: {formatC(Gamma_P, digits=9, format='f')}"))
     }
 
-    print(glue::glue("Gamma sum: {sum(gammas)}"))
-    
+    return(total_gamma)
 }
 
 
-run(
-    S0,
-    min_tick^(tick_index),
-    (min_tick^(tick_index)) - (30)
-)
-
-# 1651.8,
-# 1671.8
 
